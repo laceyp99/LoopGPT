8cdc62882c9d5fc17b7db238b9cf932f3efca06b
Patrick Lacey <pat@laceys.com>
2026-05-12 22:59:48 -0400

    fix: update bootstrap test to fit loop and time information schema

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -3,7 +3,8 @@ from mido import MidiFile
 
 def test_sample_loop_fixture_builds_valid_loop(sample_loop):
     assert sample_loop.Bar_1.notes[0].pitch == "C"
-    assert sample_loop.Bar_4.notes[0].time.start_beat == 13
+    assert sample_loop.Bar_1.notes[0].time.start_beat == 1
+    assert sample_loop.Bar_1.notes[0].time.duration == 16
 
 
 def test_midi_builder_writes_midi_file(sample_loop, midi_builder):
